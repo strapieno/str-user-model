b8da9adf32aef9ed4b011f5c3e0b9993563ea029
visa4 <antonino.visalli@gmail.com>
2015-10-24 15:12:38 +0200

    Remove api hydrator

--- config/hydrator.config.php
+++ config/hydrator.config.php
@@ -2,8 +2,6 @@
 return [
     'invokables' => [
         'Strapieno\User\Model\Hydrator\Mongo\UserModelMongoHydrator'
-            => 'Strapieno\User\Model\Hydrator\Mongo\UserModelMongoHydrator',
-        'Strapieno\User\Model\Hydrator\UserHydrator'
-            => 'Strapieno\User\Model\Hydrator\UserHydrator'
+            => 'Strapieno\User\Model\Hydrator\Mongo\UserModelMongoHydrator'
     ]
 ];
