a32626f9426df91c18125476cd2486c9a70daa5e
visa4 <antonino.visalli@gmail.com>
2015-11-10 12:49:47 +0100

    Add userIdAwareTrait

---
+++ src/Entity/UserIdAwareTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Strapieno\User\Model\Entity;
+
+/**
+ * Class UserIdAwareInterface
+ */
+trait UserIdAwareInterface
+{
+    /**
+     * @var string
+     */
+    protected $userId;
+
+    /**
+     * @return string
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param string $userId
+     * @return $this
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+        return $this;
+    }
+}
